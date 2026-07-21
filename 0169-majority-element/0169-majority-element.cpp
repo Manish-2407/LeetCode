45518a79e185ed15ed72da36e9d2a7454e50c190
Manish Singh <manishsinghenggindian@gmail.com>
2026-07-21 21:37:11 +0530

    Time: 0 ms (100%), Space: 28.3 MB (36.22%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int can=nums[0];
+        int c=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==can){
+                c++;
+            }
+            else{
+                c--;
+            }
+            if(c==0){
+                can=nums[i];
+                c=1;
+            }
+        }
+        return can;
+    }
+};
